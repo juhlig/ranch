0ecf89d0cd447504a917dd79d4a387276ef9fe45
juhlig <j.uhlig@mailingwork.de>
2020-02-25 16:16:42 +0100

    test 11

--- src/ranch_conns_sup.erl
+++ src/ranch_conns_sup.erl
@@ -416,10 +416,13 @@ system_code_change({State0, CurConns, NbChildren, Sleepers}, _, OldVsn, Extra) -
 	{ok, {State1, CurConns, NbChildren, Sleepers}}.
 
 code_change({down, _}, #state{parent=Parent, ref=Ref, conn_type=ConnType, shutdown=Shutdown, transport=Transport, protocol=Protocol, opts=Opts, handshake_timeout=HandshakeTimeout, max_conns=MaxConns, logger=Logger}, _Extra) ->
-	{ok, {statex, Parent, Ref, ConnType, Shutdown, Transport, Protocol, Opts, HandshakeTimeout, MaxConns, Logger}};
+error_logger:info_msg("####### DOWN #######~n"),
+	{ok, {state, Parent, Ref, ConnType, Shutdown, Transport, Protocol, Opts, HandshakeTimeout, MaxConns, Logger}};
 code_change(_, {state, Parent, Ref, ConnType, Shutdown, Transport, Protocol, Opts, HandshakeTimeout, MaxConns, Logger}, _Extra) ->
+error_logger:info_msg("####### UP #######~n"),
 	{ok, #state{parent=Parent, ref=Ref, conn_type=ConnType, shutdown=Shutdown, transport=Transport, protocol=Protocol, opts=Opts, handshake_timeout=HandshakeTimeout, max_conns=MaxConns, logger=Logger}};
 code_change(_OldVsn, State, _Extra) ->
+error_logger:info_msg("####### OTHER ####### ~p ~p ~p~n", [_OldVsn, State, _Extra]),
 	{ok, State}.
 
 %% We use ~999999p here instead of ~w because the latter doesn't
