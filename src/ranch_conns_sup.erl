8ed54d5d5e8c381b39e18812fdb303b8f96f07e4
juhlig <j.uhlig@mailingwork.de>
2020-02-25 14:41:48 +0100

    test 6

--- src/ranch_conns_sup.erl
+++ src/ranch_conns_sup.erl
@@ -422,6 +422,7 @@ code_change("2.0.0-rc.3", {state, Parent, Ref, ConnType, Shutdown, Transport, Pr
 error_logger:info_msg("UPGRADING"),
 	{ok, #state{parent=Parent, ref=Ref, conn_type=ConnType, shutdown=Shutdown, transport=Transport, protocol=Protocol, opts=Opts, handshake_timeout=HandshakeTimeout, max_conns=MaxConns, logger=Logger}};
 code_change(_OldVsn, State, _Extra) ->
+error_logger:info_msg("~p ~p ~p", [_OldVsn, State, Extra]),
 	{ok, State}.
 
 %% We use ~999999p here instead of ~w because the latter doesn't
